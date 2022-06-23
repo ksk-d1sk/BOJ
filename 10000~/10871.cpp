0a7ec30bf8a338a9a53be31bf677b69a2b0ad6c8
BhaT <85089251+BhaT844@users.noreply.github.com>
2022-06-23 09:56:42 +0900

    BOJ

---
+++ 10000~/10871.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	int N, X;
+	std::cin >> N >> X;
+
+	int* A = new int[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> A[i];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (A[i] < X)
+		{
+			std::cout << A[i] << ' ';
+		}
+	}
+
+	delete[] A;
+
+	return 0;
+}
