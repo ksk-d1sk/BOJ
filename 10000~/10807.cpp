3b0a30f71b542b605001bdc1de0236a427e13ca8
BhaT <85089251+BhaT844@users.noreply.github.com>
2022-06-23 09:48:12 +0900

    BOJ

---
+++ 10000~/10807.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(void)
+{
+	short n;
+	std::cin >> n;
+	
+	short* arr = new short[n];
+	
+	for (int i = 0; i < n; i++)
+		std::cin >> arr[i];
+	
+	short v;
+	std::cin >> v;
+	
+	short count = 0;
+	for (int i = 0; i < n; i++)
+		if (v == arr[i]) count++;
+	
+	delete[] arr;
+	
+	std::cout << count;
+	
+	return 0;
+} 
