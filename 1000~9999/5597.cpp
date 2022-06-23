802d75b32a5d14ffff956121265e9e970613418a
BhaT <85089251+BhaT844@users.noreply.github.com>
2022-06-23 09:46:15 +0900

    BOJ

---
+++ 1000~9999/5597.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+    
+    short arr[30];
+    
+    for (int i = 0; i < 30; i++)
+		arr[i] = 1;
+	
+	for (int i = 0; i < 28; i ++)
+	{
+		int n;
+		std::cin >> n;
+		arr[n - 1] = 0;
+	}
+	
+	for (int i = 0; i < 30; i++)
+		if (arr[i]) std::cout << i + 1 << '\n';
+	
+	return 0;
+}
