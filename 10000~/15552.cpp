49f6db679d8f65f2882b9126180f11a93f45eef1
BhaT <85089251+BhaT844@users.noreply.github.com>
2022-06-23 09:50:48 +0900

    BOJ

---
+++ 10000~/15552.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	
+	int t;
+	std::cin >> t;
+	
+	for (int i = 0; i < t; i++)
+	{
+		int a, b;
+		std::cin >> a >> b;
+		std::cout << a + b << '\n';
+	}
+	
+	return 0;
+}
