8f7819f4187507fdd74c4e342d099780e788add3
BhaT <85089251+BhaT844@users.noreply.github.com>
2022-12-01 12:27:22 +0900

    좌표 정렬하기

---
+++ 10000~/11650.rs
@@ -0,0 +1,38 @@
+// 좌표 정렬하기
+
+use std::io::{self, Read};
+use std::fmt::Write;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().map(|i| i.parse::<i32>().unwrap());
+
+    let n = input.next().unwrap() as u32;
+    let mut coordinates = Vec::new();
+    for _ in 0..n {
+        let data = (input.next().unwrap(), input.next().unwrap());
+        coordinates.push(data);
+    }
+
+    let result = solution(coordinates);
+    print!("{}", result);
+}
+
+fn solution(mut coordinates: Vec<(i32, i32)>) -> String {
+    let mut buf = String::new();
+
+    coordinates.sort_by(|&(x1, y1), &(x2, y2)|
+        if x1 == x2 {
+            y1.cmp(&y2)
+        } else {
+            x1.cmp(&x2)
+        }
+    );
+
+    for (x, y) in coordinates {
+        writeln!(buf, "{} {}", x, y).unwrap();
+    }
+
+    buf
+}
