46b44195f3ac4e2dadb68bbfd54a9dc442dac751
BhaT844 <bluehart_777@naver.com>
2023-07-17 21:59:55 +0900

    정보섬의 대중교통

---
+++ 10000~/28113.rs
@@ -0,0 +1,25 @@
+// 정보섬의 대중교통
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace();
+
+    macro_rules! get {
+        ( $($t:ty),+ ) => { ( $(input.next().unwrap().parse::<$t>().unwrap()),+ ) };
+    }
+
+    let (_, a, b) = get!(u32, u32, u32);
+
+    print!(
+        "{}",
+        if a < b {
+            "Bus"
+        } else
+        if a == b {
+            "Anything"
+        } else {
+            "Subway"
+        }
+    )
+}
