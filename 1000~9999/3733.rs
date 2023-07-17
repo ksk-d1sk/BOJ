f1f24c6901867afaa1573832b6152c845c5de2e5
BhaT844 <bluehart_777@naver.com>
2023-07-17 21:48:39 +0900

    Shares

---
+++ 1000~9999/3733.rs
@@ -0,0 +1,17 @@
+// Shares
+
+use std::io::Read;
+use std::fmt::Write;
+
+fn main() {
+    let mut input  = String::new();
+    let mut output = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    for mut input in input.lines().map(|e| e.split_ascii_whitespace().flat_map(str::parse::<u32>)) {
+        let t = input.next().unwrap() + 1;
+        let _ = writeln!(output, "{}", input.next().unwrap() / t);
+    }
+
+    print!("{output}");
+}
