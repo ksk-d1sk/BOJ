fa568538f4f5b4d87e428cb8548f556c55a0ff9b
BhaT844 <bluehart_777@naver.com>
2023-07-17 21:57:49 +0900

    Zadanie próbne

---
+++ 1000~9999/8545.rs
@@ -0,0 +1,7 @@
+// Zadanie próbne
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    print!("{}", input.trim().chars().rev().collect::<String>());
+}
